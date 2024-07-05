e166d2ac569f7e0bf5ebc3f800c102f4b42399d5
Priyanshu Saraswat <118011902+priyanshu-saraswat@users.noreply.github.com>
2024-07-05 23:10:39 +0530

    Create FizzString.java

---
+++ solutions/fizzstring/FizzString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzString {
+
+    public static String fizzString(String str) {
+        boolean startsWithF = str.startsWith("f");
+        boolean endsWithB = str.endsWith("b");
+
+        if (startsWithF && endsWithB) {
+            return "FizzBuzz";
+        } else if (startsWithF) {
+            return "Fizz";
+        } else if (endsWithB) {
+            return "Buzz";
+        } else {
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String result = fizzString(str);
+        System.out.println("The result is: " + result);
+    }
+}
